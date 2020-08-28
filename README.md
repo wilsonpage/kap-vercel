f2ade10f8a9e702e81edb5fa1aff9546410454f7
delianides <github@delianides.com>
2020-08-28 15:38:57 -0600

    converted to vercel

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# kap-now [![Build Status](https://travis-ci.org/lucaperret/kap-now.svg?branch=master)](https://travis-ci.org/lucaperret/kap-now) [![npm](https://img.shields.io/npm/dt/kap-now.svg)](https://www.npmjs.com/package/kap-now)
+# kap-vercel [![Build Status](https://travis-ci.org/lucaperret/kap-now.svg?branch=master)](https://travis-ci.org/lucaperret/kap-now) [![npm](https://img.shields.io/npm/dt/kap-now.svg)](https://www.npmjs.com/package/kap-now)
 
-![Now logo](now.png?raw=true "now.sh")
+![Vercel logo](https://www.finsmes.com/wp-content/uploads/2020/04/vercel.png "vercel.com")
 
-> [Kap](https://github.com/wulkano/kap) plugin - Share on [▲ZEIT now](https://zeit.co/now)
+> [Kap](https://github.com/wulkano/kap) plugin - Share on [Vercel](https://vercel.com)
 
 ## Install
 
 In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and click `Install`.
 
-You should also add your [Now token](https://zeit.co/account/tokens) to the plugin configuration.
-In the `Plugins` pane, click `Open plugins folder`, edit the `kap-now.json` file to add a `token` property with your token value.
+You should also add your [Vercel token](https://vercel.com/account/tokens) to the plugin configuration.
+In the `Plugins` pane, click `Open plugins folder`, edit the `kap-vercel.json` file to add a `token` property with your token value.
 
 ## Usage
 
-In the editor, after recording, select one of the export formats, and then `Share on Now`.
+In the editor, after recording, select one of the export formats, and then `Share on Vercel`.
 
 ## License
 
-MIT © [Luca Perret](https://github.com/lucaperret)
+MIT © [Luca Perret](https://github.com/lucaperret),[Drew
+Delianides](https://github.com/delianides)
--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-'use strict';
-const {createHash} = require('crypto');
-const through2 = require('through2');
-const {readFile} = require('fs-extra');
+"use strict";
+const { createHash } = require("crypto");
+const through2 = require("through2");
+const { readFile } = require("fs-extra");
 
 const action = async context => {
-	const uploadEndpoint = 'https://api.vercel.co/v12/now/files';
-	const deploymentEndpoint = 'https://api.vercel.co/v2/now/deployments';
-	const Authorization = 'Bearer ' + context.config.get('token');
+	const uploadEndpoint = "https://api.vercel.com/v2/now/files";
+	const deploymentEndpoint = "https://api.vercel.com/v12/now/deployments";
+	const Authorization = "Bearer " + context.config.get("token");
 
 	const filePath = await context.filePath();
 	const data = await readFile(filePath);
-	const sha = createHash('sha1').update(data).digest('hex');
+	const sha = createHash("sha1")
+		.update(data)
+		.digest("hex");
 	const stream = through2();
 	stream.write(data);
 	stream.end();
 
-	context.setProgress('Uploading…');
+	context.setProgress("Uploading…");
 	await context.request(uploadEndpoint, {
 		headers: {
-			'Content-Type': 'application/octet-stream',
-			'x-now-digest': sha,
-			'x-now-size': data.length,
+			"Content-Type": "application/octet-stream",
+			"x-now-digest": sha,
+			"x-now-size": data.length,
 			Authorization
 		},
 		body: stream
 	});
 
-	context.setProgress('Creating Vercel deployment…');
+	context.setProgress("Creating Vercel deployment…");
 	const deploymentResponse = await context.request(deploymentEndpoint, {
 		headers: {
 			Authorization
 		},
 		json: true,
 		body: {
-			name: context.config.get('name'),
+			name: context.config.get("name"),
 			files: [
 				{
 					file: context.defaultFileName,
 					sha,
 					size: data.length
 				}
-			]
+			],
+			projectSettings: {
+				framework: null
+			}
 		}
 	});
 
 	context.copyToClipboard(`https://${deploymentResponse.body.url}`);
-	context.notify(`URL to the ${context.prettyFormat} has been copied to the clipboard`);
+	context.notify(
+		`URL to the ${context.prettyFormat} has been copied to the clipboard`
+	);
 };
 
-const now = {
-	title: 'Share on Vercel',
-	formats: ['gif', 'mp4', 'webm', 'apng'],
+const vercel = {
+	title: "Share on Vercel",
+	formats: ["gif", "mp4", "webm", "apng"],
 	action,
 	config: {
 		token: {
-			title: 'Now token',
-			description: 'Create one here https://vercel.com/account/tokens',
-			type: 'string',
+			title: "Vercel Personal Access Token",
+			description: "Create one here https://vercel.com/account/tokens",
+			type: "string",
 			required: true
 		},
 		name: {
-			title: 'Name used in the deployment URL',
-			type: 'string',
-			default: 'kapture',
+			title: "Name used in the deployment URL",
+			type: "string",
+			default: "kapture",
 			required: true
 		}
 	}
 };
 
-exports.shareServices = [now];
+exports.shareServices = [vercel];
